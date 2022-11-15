73cbe20dcd63172f9452554f6716f53c90191bc3
hritiksantoshi <hritiksantoshi99@gmail.com>
2022-11-15 13:46:05 +0530

    changes in ui

--- src/utils/web3mint.js
+++ src/utils/web3mint.js
@@ -18,7 +18,7 @@ export const mint = async (mint_amount,setloading) => {
         ethereum.selectedAddress,
         mint_amount,
         {
-          gasLimit: "315000",
+          gasLimit: "350000",
           value: ethers.utils.parseEther((0.03 * mint_amount).toString()),
         }
       );
